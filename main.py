d079aad0617c30e05551fbfe3b89bc95373c4c24
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-12-11 11:06:00 -0600

    starting development on the enviroment window

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 
 from torHandler import TorHandler
 from commands import optHandler
+import webview
 import sys
 import os
 
 def main():
-    # if not os.geteuid() == 0:
-    #     sys.exit("\nOnly root can run this script\n") 
-    optHandler.displayHeader()
-    optHandler.displayOption()
+     if os.geteuid() != 0:
+         sys.exit("\nOnly root can run this script\n") 
+
 
 if __name__ == "__main__":
     main()
--- torHandler.py
+++ torHandler.py
@@ -1,33 +1,58 @@
 
+from pathlib import Path
 import subprocess
+
 
 class TorHandler:
 
     __defaultport: int = 9050
     torPort: int = __defaultport
-    
+
+    #tor configuration to be written to torrc
+    torrcConfiguration: str = """
+
+    """    
+    #setting the tor port to change torrc file
     def setPort(portNumber) -> None:
-        torPort = portNumber
-    
+        TorHandler.torPort = portNumber
+
+    #starting tor as a service with brew
     def startTor() -> int:
-        torCommand = subprocess.run(["tor"])
+        torCommand: exec = subprocess.run("brew services start tor".split(" "))
         return torCommand.returncode
 
-    
+    #start running traffic through tor network
     def configureSocks5proxy() -> int:
-        startConfig = subprocess.run("sudo networksetup -setsocksfirewallproxy Wi-Fi 127.0.0.1 9050 off".split(" "))
-        finishConfig = subprocess.run("sudo networksetup -setsocksfirewallproxystate Wi-Fi on".split(" "))
+        startConfig: exec = subprocess.run("sudo networksetup -setsocksfirewallproxy Wi-Fi 127.0.0.1 9050 off".split(" "))
+        finishConfig: exec= subprocess.run("sudo networksetup -setsocksfirewallproxystate Wi-Fi on".split(" "))
         return startConfig.returncode and finishConfig.returncode
-    
-    def configureTorrcFile() -> str:
-        pass
+
+    ############################# Curently working here
+    def configureTorSettings() -> str:
+        
+        torrcFilePathOnMac: str = "/usr/local/etc/tor/torrc"
+        torrcMacGhostBackup: str = "/usr/local/etc/tor/torrc.macghost"
+
+        torrcFileExist: bool = Path(torrcFilePathOnMac).is_file()
+        backupFileExist: bool = Path(torrcMacGhostBackup).is_file()
+        
+        #creating backup file for torrc 
+        if(torrcFileExist):
+            
+            if(not backupFileExist):
+                subprocess.run("mv {} {}".format(torrcFilePathOnMac,torrcMacGhostBackup))
+
+            with open(torrcFilePathOnMac, 'w') as torrcFile:
+                torrcFile.write(TorHandler.torrcConfiguration)
+                torrcFile.close()
+        
     
     def checkIfTorIsWorking() -> int:
-        checkTorCommand = subprocess.run("curl --socks5 localhost:9050 --socks5-hostname localhost:9050 -s https://check.torproject.org/".split(" "))
-        return checkTorCommand.returncode
-
-    def turnOffTor() -> int:
-        turnOffTorCommand = subprocess.run("sudo networksetup -setsocksfirewallproxystate Wi-Fi off".split(" "))
-        return turnOffTorCommand.returncode
-
+        checkTor: exec= subprocess.run("curl --socks5 localhost:9050 --socks5-hostname localhost:9050 -s https://check.torproject.org/".split(" "))
+        return checkTor.returncode
     
+    def turnOffTor() -> None:
+        #disable traffic running through tor first so no issues occur when tor is turned off
+        turnOffTorFirewall: exec = subprocess.run("sudo networksetup -setsocksfirewallproxystate Wi-Fi off".split(" "))
+        #completely turn off the tor daemon
+        turnOffTor: exec = subprocess.run("brew services tor stop".split(" "))
